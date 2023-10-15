199f94f73839d8695415447d7950bfae23e53480
MareMare <MareMare@users.noreply.github.com>
2023-10-16 02:10:46 +0900

    プロジェクト ファイルを追加します。

---
+++ src/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace ExternalConnectionStringWinFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.propertyGrid1 = new System.Windows.Forms.PropertyGrid();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(140, 353);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // propertyGrid1
+            // 
+            this.propertyGrid1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.propertyGrid1.Location = new System.Drawing.Point(140, 0);
+            this.propertyGrid1.Name = "propertyGrid1";
+            this.propertyGrid1.Size = new System.Drawing.Size(247, 353);
+            this.propertyGrid1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 353);
+            this.Controls.Add(this.propertyGrid1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PropertyGrid propertyGrid1;
+    }
+}
+
---
+++ src/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;
+using System.Windows.Forms;
+
+namespace ExternalConnectionStringWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            var settings = ConfigurationManager.ConnectionStrings;
+            foreach (ConnectionStringSettings setting in settings)
+            {
+                var info = new DisplayInfo(setting);
+                this.listBox1.Items.Add(info);
+            }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e) => this.propertyGrid1.SelectedObject = this.listBox1.SelectedItem;
+
+        private sealed class DisplayInfo
+        {
+            public DisplayInfo(ConnectionStringSettings setting) => this.Setting = setting;
+            public ConnectionStringSettings Setting { get; }
+            public override string ToString() => this.Setting.Name;
+        }
+    }
+}
